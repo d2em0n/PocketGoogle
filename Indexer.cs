5ce732ba69608fd2ce78d5330c12dc0902791868
Dmitriy Bessarab <bessarabds@gmail.com>
2024-06-13 17:39:12 +0500

    Add project files.

---
+++ Indexer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketGoogle;
+
+public class Indexer : IIndexer
+{
+    private readonly Dictionary<string, Dictionary<int, List<int>>> Data;
+
+    public Indexer()
+    {
+        Data = new Dictionary<string, Dictionary<int, List<int>>>();        
+    }
+
+    public void Add(int id, string documentText)
+    {
+        var position = 0;
+        foreach (var word in documentText.Split(new char[] { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' }))
+        {
+            if (!Data.ContainsKey(word))
+                Data[word] = new Dictionary<int, List<int>>() { { id, new List<int>() { position } } };
+            else
+            {
+                if (!Data[word].ContainsKey(id))
+                    Data[word][id] = new List<int> { position };
+                else
+                    Data[word][id].Add(position);
+            }
+            position += word.Length + 1;
+        }
+    }
+
+    public List<int> GetIds(string word)
+    {
+        if (Data.ContainsKey(word))
+            return Data[word].Keys.ToList();
+        return new List<int>();
+    }
+
+    public List<int> GetPositions(int id, string word)
+    {
+        if (Data.ContainsKey(word) && Data[word].ContainsKey(id))
+            return Data[word][id];
+        return new List<int>();
+    }
+
+    public void Remove(int id)
+    {
+        foreach (var word in Data.Keys)
+            if (Data[word].ContainsKey(id))
+                Data[word].Remove(id);
+    }
+}
---
+++ IndexerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketGoogle
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]       
+        public void SingleTest()
+        {
+            var test = new Indexer();
+            test.Add(0, "A B C");
+            test.Add(1, "B C");
+            test.Add(2, "A C A C");
+            test.Add(3, "F, f ff");
+
+            Assert.AreEqual(new List<int>() { 0, 2 } , test.GetIds("A"));
+        }
+    }
+}
